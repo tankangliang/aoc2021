39a1a58f94a29d2956e546b651d16345065f8855
tankangliang <tankangliang98@gmail.com>
2021-12-07 14:50:50 +0800

    day 7

---
+++ src/bin/7_2.rs
@@ -0,0 +1,44 @@
+use aoc2021::input_data;
+
+fn main() {
+    let crabs: Vec<usize> = input_data(7)
+        .trim()
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
+
+    let max_crab = crabs.iter().max().unwrap();
+
+    let mut crab_positions = vec![0; max_crab + 1];
+
+    for crab in &crabs {
+        crab_positions[*crab] += 1;
+    }
+
+    let mut sum_fuel_costs = vec![0; max_crab + 1];
+
+    let mut crabs_in_hand = 0;
+    let mut fuel_count = 0;
+    let mut inc_fuel_count = 0;
+
+    for (i, crabs) in crab_positions.iter().enumerate() {
+        inc_fuel_count += crabs_in_hand;
+        fuel_count += inc_fuel_count;
+        crabs_in_hand += crabs;
+        sum_fuel_costs[i] += fuel_count;
+    }
+
+    crabs_in_hand = 0;
+    fuel_count = 0;
+    inc_fuel_count = 0;
+
+    for (i, crabs) in crab_positions.iter().rev().enumerate() {
+        let idx = max_crab - i;
+        inc_fuel_count += crabs_in_hand;
+        fuel_count += inc_fuel_count;
+        crabs_in_hand += crabs;
+        sum_fuel_costs[idx] += fuel_count;
+    }
+
+    println!("{}", sum_fuel_costs.iter().min().unwrap())
+}
